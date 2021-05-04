a70279b99a621b431308a16cc262ab0f29f079a8
80010-09-14 <80010-09-14@exchange.ad.afpanet>
2021-05-04 13:53:09 +0200

    procédures stockées exercice 3 correction

--- week_18/procedures_stockees/Ex3/procedures_stockees_ex3.sql
+++ week_18/procedures_stockees/Ex3/procedures_stockees_ex3.sql
@@ -6,22 +6,25 @@
 
 DROP PROCEDURE IF EXISTS CA_Supplier;
 
-DELIMITER |
+DELIMITER $$
 
 CREATE PROCEDURE CA_Supplier(
     p_supplier_code INT(10),
     p_annee INT(4) 
 )
-
 BEGIN
-    SELECT sup_id, sup_name, ROUND(SUM((ode_unit_price-(ode_unit_price*ode_discount/100))*ode_quantity),2) AS CA
-    FROM suppliers
-    JOIN products ON sup_id = pro_sup_id
-    JOIN orders_details ON pro_id = ode_pro_id
-    JOIN orders ON ord_id = ode_ord_id
-    WHERE sup_id = p_supplier_code AND p_annee = YEAR(ord_order_date);
-
-END |
+    IF p_supplier_code IN (SELECT sup_id FROM suppliers)
+        THEN    
+            SELECT sup_id, sup_name, ROUND(SUM((ode_unit_price-(ode_unit_price*ode_discount/100))*ode_quantity),2) AS CA
+            FROM suppliers
+            JOIN products ON sup_id = pro_sup_id
+            JOIN orders_details ON pro_id = ode_pro_id
+            JOIN orders ON ord_id = ode_ord_id
+            WHERE sup_id = p_supplier_code AND p_annee = YEAR(ord_order_date);
+        ELSE
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "Le code fournisseur n'est pas valide";
+    END IF;
+END $$
 DELIMITER ;
 
-CALL CA_Supplier (1,2020);
+CALL CA_Supplier (6,2020);
