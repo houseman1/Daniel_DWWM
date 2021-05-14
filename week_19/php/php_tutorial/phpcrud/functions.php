196ae44eb97591df0609c98ad5f7cb215350046d
houseman1 <79094235+houseman1@users.noreply.github.com>
2021-05-14 08:35:04 +0200

    14/05 crud tutorial not functioning as expected

--- week_19/php/php_tutorial/phpcrud/functions.php
+++ week_19/php/php_tutorial/phpcrud/functions.php
@@ -1,1 +1,50 @@
+<!-- This file will contain functions that we can execute in all our PHP files, this is so we don't have to write the same code in every PHP file,
+shorter the code, the better, right? We'll create 3 functions, 1 function will connect to the database, the other 2 will be the templates for the header and footer
+that will appear on every page we create and will contain the HTML layout. -->
+
+
+<!--function to connect to the database
 <?php
+function pdo_connect_mysql(): PDO
+{
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_USER = 'root';
+    $DATABASE_PASS = "";
+    $DATABASE_NAME = 'phpcrud';
+    try {
+        return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
+    } catch (PDOException $exception) {
+        // If there is an error with the connection, stop the script and display the error.
+        exit('Failed to connect to database!');
+    }
+}
+
+//functions to display header and footer
+function template_header($title) {
+//heredoc string syntax for large volumes of text (<<<EOT)
+echo <<<EOT
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>$title</title>
+        <link href="style.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    </head>
+    <body>
+    <nav class="navtop">
+        <div>
+            <h1>Website Title</h1>
+            <a href="index.php"><i class="fas fa-home"></i>Home</a>
+            <a href="read.php"><i class="fas fa-address-book"></i>Contacts</a>
+        </div>
+    </nav>
+EOT;
+}
+function template_footer() {
+echo <<<EOT
+    </body>
+</html>
+EOT;
+}
+?>
--- week_19/php/php_tutorial/phpcrud/index.php
+++ week_19/php/php_tutorial/phpcrud/index.php
@@ -1,3 +1,6 @@
+<!--  tutorial address
+https://codeshack.io/crud-application-php-pdo-mysql/  -->
+
 <!--What each file contains:
 index.php      - Home page for my CRUD app
 create.php     - Create new records with an HTML form and send data to the server with a POST request.
@@ -5,5 +8,22 @@ read.php       - Display records from our database table and navigate with pagin
 update.php     - Update existing records with an HTML form and send data to the server with a POST request.
 delete.php     - Confirm and delete records by ID (GET request to the get the ID).
 functions.php  -  Basic templating functions and MySQL connection function (so we don't have to repeat code in every file).
-style.css      -  The sylesheet for our app, this will change the appearance of our app.
+style.css      -  The sylesheet for our app, this will change the appearance of our app.-->
+
+<!--When you navigate to https://localhost/phpcrud/ it will serve the index.php file
+/** @noinspection PhpVoidFunctionResultUsedInspection */
 <?php
+include 'functions.php';
+// my php code here
+
+// Home page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+    <h2>Home</h2>
+    <p>Welcome to the home page!</p>
+</div>
+
+<?=template_footer()?>
